744bb7d7c8e30367c6398d500567745e636afdcc
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-09 13:51:54 +0200

    Update RootLayout.jsx

--- client/src/pages/RootLayout.jsx
+++ client/src/pages/RootLayout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import Navbar from '../components/Navbar'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <>
+    <Navbar />
+    <Outlet />
+    </>
   )
 }
 
