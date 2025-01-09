ffcf96be8409e119307a73c483ee9e4455e637da
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-09 16:17:31 +0200

    Update ErrorPage.jsx

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Image from '../assets/error2.gif'
+import { useNavigate } from 'react-router-dom'
 
 const ErrorPage = () => {
+  const navigate = useNavigate()
+
+  // redirect user to previous page
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(-1)
+    }, 6000)
+  })
+
+
   return (
-    <div>ErrorPage</div>
+    <section className="errorPage">
+      <div className="errorPage__container">
+        <img src={Image} alt="Page not found" />
+        <h1>404</h1>
+        <p>This page does not exist. You will be redirected to the previous page shortly.</p>
+      </div>
+    </section>
   )
 }
 
