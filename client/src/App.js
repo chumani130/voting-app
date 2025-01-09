346ca4b2d1da75da8c060401ec763f9e68d7fc56
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-09 16:17:18 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from './pages/RootLayout'
 import ErrorPage from './pages/ErrorPage'
 import Login from './pages/Login'
+import Results from './pages/Register'
+import Elections from './pages/Elections'
+import ElectionsDetails from './pages/ElectionDetails'
+import Candidates from './pages/Candidates'
+import Congrats from './pages/Congrats'
+import Logout from './pages/Logout'
+import Register from './pages/Register'
 
 const router = createBrowserRouter([
   {
@@ -14,6 +21,37 @@ const router = createBrowserRouter([
          index: true,
          element: <Login/>
       },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "results",
+        element: <Results />
+      },
+      {
+        path: "elections",
+        element: <Elections />
+      },
+      {
+        path: "elections/:id",
+        element: <ElectionsDetails />
+      },
+      {
+        path: "elections/:id/candidates",
+        element: <Candidates />
+      },
+      {
+        path: "congrats",
+        element: <Congrats />
+      },
+      {
+        path: "logout",
+        element: <Logout />
+      },
+      
+      
+      
 
     ]
   }
