1fc183b3ffae6c57ab0673dcd5106fb9f51b01cf
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-09 13:51:41 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import RootLayout from './pages/RootLayout'
+import ErrorPage from './pages/ErrorPage'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout/>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+         index: true,
+         element: <Login/>
+      },
+
+    ]
+  }
+])
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <RouterProvider router={router} />
   );
 }
 
