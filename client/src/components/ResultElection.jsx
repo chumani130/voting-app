f3c65897be6ed9c19b1e8e77c7bafea849c5fc92
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-13 13:00:51 +0200

    Update ResultElection.jsx

---
+++ client/src/components/ResultElection.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { candidates } from '../data'
+import CandidateRating from './CandidateRating'
+
+const ResultElection = (id, thumbnail, title) => {
+
+    const [totalVotes, setTotalVotes] = useState
+
+    //get candidates that belong to this election iteration
+    const electionCandidates = candidates.filter(candidate => {
+        return candidate.election == id
+    })
+
+  return (
+    <article className="result">
+        <header className="result_header">
+            <h4>{title}</h4>
+            <div className="result__header-image">
+                <img src={thumbnail} alt={title} />
+            </div>
+            <ul className="result__list">
+                {
+                    electionCandidates.map(candidate => <CandidateRating key=
+                    {candidate.id} {...candidate} totalVotes={totalVotes} />)
+                }
+            </ul>
+        </header>
+    </article>
+  )
+}
+
+export default ResultElection
