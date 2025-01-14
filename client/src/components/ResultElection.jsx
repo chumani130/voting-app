122bdfa1e35fa6866a1b77295fc7c6f5c2867f9c
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-14 13:49:35 +0200

    Update ResultElection.jsx

--- client/src/components/ResultElection.jsx
+++ client/src/components/ResultElection.jsx
@@ -2,26 +2,23 @@ import React, { useState } from 'react'
 import { candidates } from '../data'
 import CandidateRating from './CandidateRating'
 
-const ResultElection = (id, thumbnail, title) => {
-
-    const [totalVotes, setTotalVotes] = useState
+const ResultElection = ({id, thumbnail, title}) => {
+    const [totalVotes, setTotalVotes] = useState(521)
 
     //get candidates that belong to this election iteration
-    const electionCandidates = candidates.filter(candidate => {
-        return candidate.election == id
-    })
+    const electionCandidates = candidates.filter(candidate => candidate.election == id)
 
   return (
     <article className="result">
-        <header className="result_header">
+        <header className="result__header">
             <h4>{title}</h4>
             <div className="result__header-image">
                 <img src={thumbnail} alt={title} />
             </div>
             <ul className="result__list">
                 {
-                    electionCandidates.map(candidate => <CandidateRating key=
-                    {candidate.id} {...candidate} totalVotes={totalVotes} />)
+                    electionCandidates.map(candidate => (<CandidateRating key=
+                    {candidate.id} {...candidate} totalVotes={totalVotes} />))
                 }
             </ul>
         </header>
