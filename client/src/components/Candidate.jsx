c1d6cb43b5a63b025673a3c45687090ef01de738
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-14 16:10:10 +0200

    Create Candidate.jsx

---
+++ client/src/components/Candidate.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Candidate = ({image, id, fullName, motto}) => {
+  return (
+    <article className='candidate'>
+        <div className="candidate__image">
+            <img src={image} alt={fullName} />
+        </div>
+        <h5>{fullName.length > 20 ? fullName.substring(0, 20) + "..." : fullName}</h5>
+        <small>{motto.length > 20 ? motto.substring(0, 25) + "..." : motto}</small>
+        <button className="btn primary">Vote</button>
+
+    </article>
+  )
+}
+
+export default Candidate
