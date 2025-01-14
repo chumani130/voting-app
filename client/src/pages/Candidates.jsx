2f43a5d74f038067967503b3d0ea23a7834614f5
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-14 16:10:14 +0200

    Update Candidates.jsx

--- client/src/pages/Candidates.jsx
+++ client/src/pages/Candidates.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { candidates as dummyCandidates} from '../data'
+import { useParams } from 'react-router-dom'
+import Candidate from '../components/Candidate'
 
 const Candidates = () => {
+  const {id} = useParams()
+
+  // get candidates that belong to this election
+  const candidates = dummyCandidates.filter(candidate => candidate.election == id)
+
   return (
-    <div>Candidates</div>
+    <section className="candidates">
+      <header className="candidates__header">
+        <h1>Vote for your candidate</h1>
+        <p>These are the candidates for the selected election. Please vote once and wisely, because you won't be allowed to e in this election again</p>
+      </header>
+      <div className="container candidates__container">
+        {
+          candidates.map(candidate => <Candidate key={candidate.id} {...candidate}/>)
+        }
+
+      </div>
+    </section>
   )
 }
 
