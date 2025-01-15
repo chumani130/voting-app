05f5370788a7e0f16f9d8b6a60d44cc0bfbae0ee
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-15 13:55:25 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import store from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider><App /></Provider>
+    <Provider store={store}><App /></Provider>
 );
 
