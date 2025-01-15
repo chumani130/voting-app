7f9521f69c2a8c22a1aea39f27e96fe470bf9a24
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-15 15:29:43 +0200

    Update Candidates.jsx

--- client/src/pages/Candidates.jsx
+++ client/src/pages/Candidates.jsx
@@ -3,9 +3,13 @@ import { candidates as dummyCandidates} from '../data'
 import { useParams } from 'react-router-dom'
 import Candidate from '../components/Candidate'
 import ConfirmVote from '../components/ConfirmVote'
+import { useSelector } from 'react-redux'
 
 const Candidates = () => {
   const {id} = useParams()
+
+  const voteCandidateModalShowing = useSelector(state => state.ui.voteCandidateModalShowing)
+
 
   // get candidates that belong to this election
   const candidates = dummyCandidates.filter(candidate => candidate.election == id)
@@ -26,7 +30,7 @@ const Candidates = () => {
 
         </div>
       </section>
-      <ConfirmVote />
+      {voteCandidateModalShowing && <ConfirmVote />}
     </>
   )
 }
