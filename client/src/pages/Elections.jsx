16ecdc577d260969b06e5716ff9f13e3628ee3be
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-15 15:57:11 +0200

    Update Elections.jsx

--- client/src/pages/Elections.jsx
+++ client/src/pages/Elections.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { elections as dummyElections }from '../data'
+import Election from '../components/Election'
 
 const Elections = () => {
+
+  const [elections, setElections] = useState(dummyElections)
   return (
-    <div>Elections</div>
+    <section className="elections">
+      <header className="container elections__container">
+        <h1>Ongoing Elections</h1>
+        <button className="btn primary">Create New Election</button>
+      </header>
+      <menu className="election__menu">
+        {
+          elections.map(election => <Election key={election.id} {...election} /> )
+        }
+      </menu>
+    </section>
   )
 }
 
