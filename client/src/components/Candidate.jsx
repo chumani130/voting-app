5919e6d100980f0bde68a1359097e0f33520f859
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-15 15:29:50 +0200

    Update Candidate.jsx

--- client/src/components/Candidate.jsx
+++ client/src/components/Candidate.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { UiActions } from '../store/ui-slice'
+
 
 const Candidate = ({image, id, fullName, motto}) => {
+
+  const dispatch = useDispatch()
+
+  // open confirm vote modal
+  const openCandidateModal = () => {
+    dispatch(UiActions.openVoteCandidateModal())
+  }
+
   return (
     <article className='candidate'>
         <div className="candidate__image">
@@ -8,7 +19,7 @@ const Candidate = ({image, id, fullName, motto}) => {
         </div>
         <h5>{fullName.length > 20 ? fullName.substring(0, 20) + "..." : fullName}</h5>
         <small>{motto.length > 20 ? motto.substring(0, 25) + "..." : motto}</small>
-        <button className="btn primary">Vote</button>
+        <button className="btn primary" onClick={openCandidateModal}>Vote</button>
 
     </article>
   )
