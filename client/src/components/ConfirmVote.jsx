238190af84d701b45dbf6c58b23f819133033fd1
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-15 13:55:30 +0200

    Update ConfirmVote.jsx

--- client/src/components/ConfirmVote.jsx
+++ client/src/components/ConfirmVote.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {candidates } from   '../data'
 
 const ConfirmVote = () => {
+
+  const [modalCandidate, setModalCandidate] = useState({})
+
+  // get the selected candidate
+  const fetchCandidate = () => {
+    candidates.find(candidate => {
+      if(candidate.id == "c1") {
+        setModalCandidate(candidate)
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchCandidate()
+  }, [])
   return (
-    <div>ConfirmVote</div>
+    <section className="modal">
+      <div className="modal__content confirm__vote-content">
+        <h5>Please confirm your vote</h5>
+        <div className="confirm__vote-image">
+          <img src={modalCandidate.image} alt={modalCandidate.fullName} />
+      
+        </div>
+        <h2>{modalCandidate?.fullName?.length > 17 ? modalCandidate?.fullName?.substring(0, 17) + "..." :
+          modalCandidate?.fullName}</h2>
+        <p>{modalCandidate?.motto?.length > 45 ? modalCandidate?.motto?.substring(0, 45) + "..." :
+          modalCandidate?.motto}</p>
+        <div className="confirm__vote-cta">
+          <button className="btn">Cancel</button>
+          <button className="btn primary">Confirm</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
