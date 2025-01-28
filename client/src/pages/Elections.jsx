30c305020d5c80ee647c306d849c740f6ce1853c
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-28 12:41:56 +0200

    Update Elections.jsx

--- client/src/pages/Elections.jsx
+++ client/src/pages/Elections.jsx
@@ -1,15 +1,29 @@
 import React, {useState} from 'react'
 import { elections as dummyElections }from '../data'
 import Election from '../components/Election'
+import AddElectionModal from '../components/AddElectionModal'
+import { useDispatch } from 'react-redux'
+import { UiActions } from '../store/ui-slice'
+import { useSelector } from 'react-redux'
 
 const Elections = () => {
 
   const [elections, setElections] = useState(dummyElections)
+
+  const dispatch = useDispatch()
+
+  // open add election modal
+  const openModal = () => {
+    dispatch(UiActions.openElectionModal())
+  }
+
+  const electionModalShowing = useSelector(state => state.ui.electionModalShowing)
   return (
+   <>
     <section className="elections">
       <header className="container elections__header">
         <h1>Ongoing Elections</h1>
-        <button className="btn primary">Create New Election</button>
+        <button className="btn primary" onClick={openModal}>Create New Election</button>
       </header>
       <menu className="elections__menu">
         {
@@ -17,6 +31,8 @@ const Elections = () => {
         }
       </menu>
     </section>
+    {electionModalShowing && <AddElectionModal />}
+   </>
   )
 }
 
