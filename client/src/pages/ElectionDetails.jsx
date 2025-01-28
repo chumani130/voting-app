21578ac0c4c8c34bd919b0d9dbc7d6ded148179f
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-28 12:41:58 +0200

    Update ElectionDetails.jsx

--- client/src/pages/ElectionDetails.jsx
+++ client/src/pages/ElectionDetails.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { elections } from '../data'
+import { useParams } from 'react-router-dom'
+import { candidates } from '../data'
+
 
 const ElectionDetails = () => {
+
+  const {id} = useParams()
+
+  const currentElection = elections.find(election => election.id === id)
+
+  const electionCandidates = candidates.filter(candidate => candidate.election === id)
+
   return (
-    <div>ElectionDetails</div>
+    <section className="electionDetails">
+      <div className="container electionDetails__container">
+        <h2>{currentElection.title}</h2>
+        <p>{currentElection.description}</p>
+        <div className="electionDetails__image">
+          <img src={currentElection.thumbnail} alt={currentElection.title} />
+        </div>
+
+        <menu className="electionDetails__candidates">
+          {
+            electionCandidates.map(candidate => <ElectionDetails key={candidate.id}
+            {...candidate} />)
+          }
+        </menu>
+      </div>
+    </section>
   )
 }
 
